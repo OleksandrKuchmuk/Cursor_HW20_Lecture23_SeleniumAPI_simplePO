652d39452096a704a97ee5ee5dbd212c230e2e87
Oleksandr Kuchmuk <96177236+OleksandrKuchmuk@users.noreply.github.com>
2022-10-05 15:19:23 +0300

    create cetelog class with web element laptopsAndComputersButton and method click

---
+++ src/main/java/pages/CatalogPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CatalogPage extends BasePage{
+    @FindBy(xpath = "//a[contains(text(), 'Ноутбуки та комп’ютери')]")
+    private WebElement laptopsAndComputersButton;
+
+    public void clickOnLaptopsAndComputersButton(){
+        laptopsAndComputersButton.click();
+    }
+
+}
