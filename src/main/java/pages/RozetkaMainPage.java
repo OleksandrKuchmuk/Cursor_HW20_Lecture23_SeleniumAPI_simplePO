c38273cc3de6d953703fcda1c5905aa4ff63bca9
Oleksandr Kuchmuk <96177236+OleksandrKuchmuk@users.noreply.github.com>
2022-10-05 15:01:11 +0300

    create rozetka main page web elements and methods

--- src/main/java/pages/RozetkaMainPage.java
+++ src/main/java/pages/RozetkaMainPage.java
@@ -4,6 +4,36 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class RozetkaMainPage extends BasePage{
+    @FindBy(xpath = "//button[@id='fat-menu']")
+    private WebElement catalogButton;
+
+    @FindBy(xpath = "//input[@name='search']")
+    private WebElement searchBar;
+
+    @FindBy(xpath = "//button[contains(text(), 'Знайти')]")
+    private WebElement searchButton;
+
+    public void clickOnCatalogBtn() {
+        catalogButton.click();
+    }
+
+    public void clickOnSearchBar() {
+        searchBar.click();
+    }
+
+    public void clearSearchBar(){
+        searchBar.clear();
+    }
+
+    public void inputQueryToSearchBar(String text){
+        searchBar.sendKeys(text);
+    }
+
+    public void pushSearchButton(){
+searchButton.click();
+    }
+
+
 
 
 
